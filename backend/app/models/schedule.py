3fec8c01bb2e13e908392246d4704613228845e3
An Phung <ptta2708@gmail.com>
2025-05-02 01:55:32 -0700

    fix import error

--- backend/app/models/schedule.py
+++ backend/app/models/schedule.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
 from sqlalchemy.orm import relationship
-from backend.app.database import Base
+from database import Base
 
 class Schedule(Base):
     __tablename__ = "schedules"
